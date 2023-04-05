85e8a05944e2aae522b2c4d10f282b3511abfc92
johngarnsey <johngarnsey@hotmail.com>
2023-04-04 21:08:56 -0400

    initial commit add files

---
+++ merklelib/__init__.py
@@ -0,0 +1,17 @@
+#VERSIONING FOR ERROR PREVENTION ----- jg
+__version__ = "1.0"
+
+__all__ = [
+    "MerkleTree",
+    "AuditProof",
+    "Hasher",
+    "verify_leaf_inclusion",
+    "verify_tree_consistency",
+    "AuditNode",
+    "beautify",
+    "jsonify",
+    "export",
+]
+
+from merklelib.merkle import *
+from merklelib.format import *
